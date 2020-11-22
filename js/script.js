131696d9e2190c473bc482691ef529573d83670c
Nichapat K <42914563+khunpanggg@users.noreply.github.com>
2020-11-22 12:52:21 +0700

    update index

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.onscroll = (event) => {
     const viewportY = window.scrollY;
     console.log(viewportY);
 
-    document.querySelector("#header-1").style.top = Math.min(viewportY / 50, 20) + "em";
+    document.querySelector("#header-1").style.top = Math.min(viewportY / 50, 20) + "vw";
 
 }
 
