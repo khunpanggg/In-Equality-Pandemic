d93f044102ff6adb78839e52cd85098599781be4
Nichapat K <42914563+khunpanggg@users.noreply.github.com>
2020-11-24 03:47:51 +0700

    :zap:  delete files

--- Web Version.1.0/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-document.onscroll = (event) => {
-
-    const viewportY = window.scrollY;
-    console.log(viewportY);
-
-    document.querySelector("#header-1").style.top = Math.min(viewportY / 50, 20) + "em";
-
-}
