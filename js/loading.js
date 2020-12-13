617c30748bb58038d38000eb694058f76cbf7222
Nichapat K <42914563+khunpanggg@users.noreply.github.com>
2020-12-14 02:35:23 +0700

    add file sdgs

--- js/loading.js
+++ js/loading.js
@@ -1,33 +1,27 @@
-/*unhiding the session part will enable browser session storage, try it.*/
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+let imagesFinishLoad = 0;
+const totalImagesToLoad = imagesToLoad.length;
 
-    /*if (!sessionStorage.isVisited) {
-      sessionStorage.isVisited = 'true';*/
-      var progress = 0;
-      document.onreadystatechange = function() {
-          if (document.readyState == "interactive") {
-              var allElement = $("*");
-              var length = allElement.length;
-              for (var i = 0; i < length; i++) {
-                  set_element(allElement[i], length);
-              }
-          }
-      }
-  
-      function set_element(element, totalElement) {
-          var percentage = 100 / totalElement;
-          if ($(element).length == 1) {
-  
-              $("#fill").animate({
-                  width: progress + percentage + "%"
-              }, 500, function() {
-                  if (document.getElementById("fill").style.width == "100%") {
-                      $(".loadingPage").fadeOut(500);
-                  }
-              });
-              progress = progress + percentage;
-          }
-      }
-      /*unhide this else part too to enable browser cookie*/
-      /*}else {
-          $(".loadingPage ").hide();
-  }*/
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    handlerImageLoadingFinish();
+    image.removeAttribute('data-src');
+  };
+};
+
+const handlerImageLoadingFinish = () => {
+  let percentage = (100 / totalImagesToLoad) * (++imagesFinishLoad);
+  $("#fill").animate({
+    width: percentage + "%"
+  }, 500, () => {
+    if (imagesFinishLoad == totalImagesToLoad || document.getElementById("fill").style.width == "100%") {
+      $(".loadingPage").fadeOut(500);
+      document.body.dataset.loading = "";
+    }
+  });
+}
+
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
--- js/sdgs-script.js
+++ js/sdgs-script.js
@@ -1,10 +1,44 @@
 $(window).scroll(parallaxScrolling);
 
 function parallaxScrolling() {
-  var scrolled = $(window).scrollTop();
-  $('.layer1').css('top', (0 - (scrolled * .88)) + 'px');
-  $('.layer2').css('top', (0 - scrolled) + 'px');
-  $('.layer3').css('top', (0 - (scrolled * .77)) + 'px');
-  $('.layer4').css('top', (0 - (scrolled * .88)) + 'px');
-  // $('.layer5').css('top', (0 - (scrolled * .22)) + 'px');
+    var scrolled = $(window).scrollTop();
+    $('.layer1').css('top', (0 - (scrolled * .88)) + 'px');
+    $('.layer2').css('top', (0 - (scrolled * .88)) + 'px');
+    $('.layer3').css('top', (0 - (scrolled * .77)) + 'px');
+    $('.layer4').css('top', (0 - (scrolled * .99)) + 'px');
+    // $('.layer5').css('top', (0 - (scrolled * .22)) + 'px');
 }
+
+    /*unhiding the session part will enable browser session storage, try it.*/
+
+    /*if (!sessionStorage.isVisited) {
+      sessionStorage.isVisited = 'true';*/
+    var progress = 0;
+    document.onreadystatechange = function() {
+        if (document.readyState == "interactive") {
+            var allElement = $("*");
+            var length = allElement.length;
+            for (var i = 0; i < length; i++) {
+                set_element(allElement[i], length);
+            }
+        }
+    }
+
+    function set_element(element, totalElement) {
+        var percentage = 100 / totalElement;
+        if ($(element).length == 1) {
+
+            $("#fill").animate({
+                width: progress + percentage + "%"
+            }, 500, function() {
+                if (document.getElementById("fill").style.width == "100%") {
+                    $(".loadingPage").fadeOut(500);
+                }
+            });
+            progress = progress + percentage;
+        }
+    }
+    /*unhide this else part too to enable browser cookie*/
+    /*}else {
+		$(".loadingPage ").hide();
+}*/
