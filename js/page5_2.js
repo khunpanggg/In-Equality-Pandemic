ee47b0fc66c6dabccdad4db35cc4fcfdb69d5c62
Nichapat K <42914563+khunpanggg@users.noreply.github.com>
2020-12-13 18:18:46 +0700

    update

--- js/page5_2.js
+++ js/page5_2.js
@@ -1,25 +1,25 @@
 
-$(document).ready(function() {
-    var lastScrollTop = 0;
+// $(document).ready(function() {
+//     var lastScrollTop = 0;
 
-    $(window).scroll(function(event) {
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop) {
-            // downscroll code
-            console.log(1)
-            if (st > 1) {
-                $("#s2").slideDown("slow");
-            } else {
+//     $(window).scroll(function(event) {
+//         var st = $(this).scrollTop();
+//         if (st > lastScrollTop) {
+//             // downscroll code
+//             console.log(1)
+//             if (st > 1) {
+//                 $("#s2").slideDown("slow");
+//             } else {
 
-            }
+//             }
 
-        } else {
-            // upscroll code
-            console.log(2)
-            $("#s2").slideUp("slow");
+//         } else {
+//             // upscroll code
+//             console.log(2)
+//             $("#s2").slideUp("slow");
 
-        }
-        lastScrollTop = st;
-        console.log(st)
-    });
-});
+//         }
+//         lastScrollTop = st;
+//         console.log(st)
+//     });
+// });
